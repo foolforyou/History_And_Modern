ec20656dee66235ff11eef32d5b512bc625bc14f
qianfeng <qianfeng@CS-2.local>
2015-09-28 12:00:06 +0800

    change

---
+++ History_And_Modern/History_And_Modern/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  History_And_Modern
+//
+//  Created by qianfeng on 15/9/28.
+//  Copyright © 2015年 陈思. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ History_And_Modern/History_And_ModernTests/History_And_ModernTests.m
@@ -0,0 +1,39 @@
+//
+//  History_And_ModernTests.m
+//  History_And_ModernTests
+//
+//  Created by qianfeng on 15/9/28.
+//  Copyright © 2015年 陈思. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface History_And_ModernTests : XCTestCase
+
+@end
+
+@implementation History_And_ModernTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
